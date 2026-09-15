07b133cdd6c604540b324d7af2307e256b576f4b
20261431 <dltndn04@gmail.com>
2026-09-15 13:49:40 +0900

    3rd파일 추가

---
+++ python/3rd.py
@@ -0,0 +1,13 @@
+##a = int(input("첫번째 값 :"))
+##b = int(input("두번째 값 :"))
+##c = int(input("세번째 값 :"))
+
+##print(a, "*", b, "*", c, "=" , a*b*c)
+##print(a, "/", b, "/", c, "=" , a/b/c)
+##print(a, "+", b, "+", c, "=" , a+b+c)
+##print(a, "-", b, "-", c, "=" , a-b-c)
+
+data = '안녕'+\
+       '하세요?' +\
+       '파이썬!'
+print(data)
